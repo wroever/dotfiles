c590e609b58130765bf66b32ae7a67bb7b5b75e7
Will Roever <william.roever@gmail.com>
2023-05-14 10:18:27 -0700

    init.lua updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -184,24 +184,8 @@ require('lazy').setup({
     end,
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  --
-  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  -- { import = 'custom.plugins' },
+  { 'nvim-treesitter/nvim-treesitter-context', opts = {} },
 }, {})
-
--- vim.cmd.colorscheme 'nord'
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
