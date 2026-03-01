dcbf532bd17b799dca8222bdd8c283ce003887f9
Will Roever <william.roever@gmail.com>
2026-02-28 20:08:52 -0800

    Update plugins, fix italics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -104,8 +104,12 @@ require("lazy").setup({
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
+      require('rose-pine').setup({
+        styles = {
+          italic = false
+        }
+      })
       vim.cmd("colorscheme rose-pine")
-      require('rose-pine').setup({disable_italics = true})
     end,
   },
 
